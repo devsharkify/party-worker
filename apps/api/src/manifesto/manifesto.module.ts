a5e77b898358c38453c03c281622fb8a02371e41
Rohan <ruthlessrohan007@gmail.com>
2026-06-26 03:39:19 +0530

    fix(manifesto): add PrismaModule to ManifestoModule imports

--- apps/api/src/manifesto/manifesto.module.ts
+++ apps/api/src/manifesto/manifesto.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma/prisma.module";
 import { SecurityModule } from "../auth/security.module";
 import { ManifestoService } from "./manifesto.service";
 import { ManifestoController } from "./manifesto.controller";
 
 @Module({
-  imports: [SecurityModule],
+  imports: [PrismaModule, SecurityModule],
   providers: [ManifestoService],
   controllers: [ManifestoController],
   exports: [ManifestoService],
